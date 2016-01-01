8b34a2c5a766b980c3cde7ed124b6e6c581cca56
JYhuang <1017431192@qq.com>
2016-01-02 02:53:58 +0800

    修改折叠触发

--- js/self/main.js
+++ js/self/main.js
@@ -2,6 +2,6 @@
  * Created by henry on 2016/1/2.
  */
 
-$("#coding").on("click",function(){
-    $(".toggle").toggle();
+$(".banner").on("click",function(){
+    $(this).children('ul').toggle();
 })
