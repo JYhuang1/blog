2a7b73fe3928f4dbb859d676459bfa466317ca95
JYhuang <1017431192@qq.com>
2016-01-04 22:47:40 +0800

    更新首页

--- js/self/main.js
+++ js/self/main.js
@@ -4,4 +4,12 @@
 
 $(".banner").on("click",function(){
     $(this).children('ul').toggle();
-})
+});
+
+//var server = "http://localhost:8080/blog/";
+//var param = {};
+//
+//param['dataJson'] = JSON.stringify({category:0});
+//$.post(server+"selectByCategory",param,function(data){
+//    console.log(data);
+//})
