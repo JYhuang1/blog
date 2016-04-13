ab659c373edd889efefb0c72abc8e1c7229d7844
huangjinyu <huangjinyu@buddydc.com>
2016-04-13 10:28:35 +0800

    优化页面展示

--- js/self/main.js
+++ js/self/main.js
@@ -6,34 +6,53 @@
 $(".banner").on("click",function(){
     $(this).children('ul').toggle();
 });
-
-//var server = "http://localhost:8080/blog/";
-var server = "http://115.28.33.164:8080/blog/";
+var server = "http://"+location.hostname+ ":8080/blog/";
+console.log(server);
 var param = {};
-function selectByTag(tag){
-    param['dataJson'] = JSON.stringify({tag:tag});
-    $.post(server+"selectByTag",param,function(data){
+function selectByTag(){
+    param['dataJson'] = JSON.stringify({author:"拾荒者"});
+    $.post(server+"selectByAuthor",param,function(data){
         var dataJson = $.parseJSON(data);
         if (dataJson.status == 0){
             var num = dataJson.data.length;
+            console.log(dataJson.data.length);
             for(var i=0;i<num;i++){
+                console.log(dataJson.data[i].tag);
+
                 $('#content').append(
                     "<div style='border-bottom: 1px solid #eeeeee;padding-bottom: 4px'>"+
-                    "<h2 style='font-size: 20px'><a href='detail.html'>"+dataJson.data[i].title+"</a></h2>"+
+                    "<h2 style='font-size: 20px'><a href='detail.html' id='uuid"+i+"'>"+dataJson.data[i].title+"</a></h2>"+
                     "<p style='margin-top: 5px;margin-bottom: 5px'>"+dataJson.data[i].brief+"</p>"+
                     "<div class='row'>" +
                     "<span class='col-sm-3 glyphicon glyphicon-thumbs-up'>&nbsp;1</span>"+
                     "<span class='col-sm-3 glyphicon glyphicon-envelope'>&nbsp;"+dataJson.data[i].comments+"</span>"+
                     "<span class='col-sm-3 glyphicon glyphicon-time'>&nbsp;"+dateToString(dataJson.data[i].createTime)+"</span>"+
-                    "<span class='col-sm-3 glyphicon glyphicon-tag'>&nbsp;前端</span>"+
+                    "<span class='col-sm-3 glyphicon glyphicon-tag'>&nbsp;"+getTagNameByTagNo(dataJson.data[i].tag)+"</span>"+
                     "</div>"+
                     "</div>"
                 );
+                document.getElementById("uuid"+i+"").value=dataJson.data[i].uuid;
             }
+            $("a[id^='uuid']").click(function () {
+                document.cookie="articleUuid="+document.getElementById($(this).attr("id")).value;
+            });
         }else{
             alert(dataJson.message);
         }
     })
+}
+function getTagNameByTagNo(tagNo){
+
+    var tagName = null;
+    param['dataJson'] = JSON.stringify({tagNo:tagNo})
+    $.ajaxSetup({
+        async: false
+    });
+    $.post(server+"getTagNameByTagNo",param,function(data){
+        tagName = $.parseJSON(data).data;
+    });
+    console.log(tagName);
+    return tagName;
 }
 
 function dateToString(longDate){
